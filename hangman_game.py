764f14808d2d530dee38d867106c5c73e4cd08ff
shivam khode <130055547+shivam-khode01@users.noreply.github.com>
2023-10-26 00:07:28 +0530

    Add files via upload

--- hangman_game.py
+++ hangman_game.py
@@ -5,7 +5,29 @@ letter=["apple","banana","orange"]
 display=[]
 life=0
 word=random.choice(letter)
-print(word)
+if word=="apple":
+    print("HINT!! I'm a fruit, crisp and sweet,\n"
+        "In pies and tarts, I'm quite a treat.\n"
+        "I'm red or green, a healthy snack,\n"
+        "A teacher's gift, a tree to track.\n"
+        "What am I that you can find in a grove?\n" 
+        "A word of five letters, can you solve? ")
+if word=="banana":
+    print("I'm a fruit with a peel so yellow,\n"
+        "In bunches, I dangle, a cheerful fellow.\n"
+        "I'm creamy inside, a tropical delight,\n"
+        "In smoothies and splits, I'm just right.\n"
+        "What am I that monkeys often adore?\n"
+        "A word of six letters, can you explore?\n")
+if word=="orange":
+    print("I'm a citrus fruit, so round and bright\n,"
+        "My color's like a sunset's light.\n"
+        "When you're thirsty, I'm a juicy cure,\n"
+        "With segments inside, so pure.\n"
+        "What am I with a zesty tang to savor?\n"
+        "A word of six letters, can you flavor?")
+
+#print(word)
 for i in range(len(word)):
     display+='_'
 print(display)
